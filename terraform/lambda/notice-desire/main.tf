3143ff5cd299a1443b4e26c6e769455e39dc8bc9
amatyrain <107156393+amatyrain@users.noreply.github.com>
2024-02-23 09:34:30 +0900

    refactor(lambda notice-desire): ハンドラーを変更

--- terraform/lambda/notice-desire/main.tf
+++ terraform/lambda/notice-desire/main.tf
@@ -18,7 +18,7 @@ module "notice-desire" {
   event_bridge_rule_name = "hourly"
   aws_lambda_function_name = "notice-desire"
   aws_lambda_layer_arn = "arn:aws:lambda:ap-northeast-1:408633466991:layer:amazonAffiliateLayer:16"
-  aws_lambda_handler = "lambda_function.notice_desire"
+  aws_lambda_handler = "lambda_function.lambda_handler"
   aws_lambda_timeout = 600
   source_dir = "${path.module}/../../../../../src"
   output_zip_path = "${path.module}/../../../../../local/function.zip"
