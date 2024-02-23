8db7fc1dfef75c6622fe50e887d43be9f43b41f1
amatyrain <107156393+amatyrain@users.noreply.github.com>
2024-02-23 09:07:44 +0900

    fix: env_to_json.sh

--- env_to_json.sh
+++ env_to_json.sh
@@ -12,7 +12,7 @@ set +a
 # 環境変数をJSONに変換
 cat $secret_env | \
     grep -v "=$" | \
-    grep -v "AWS_ACCESS_KEY_ID" | \
-    grep -v "AWS_SECRET_ACCESS_KEY" | \
+    # grep -v "AWS_ACCESS_KEY_ID" | \
+    # grep -v "AWS_SECRET_ACCESS_KEY" | \
     grep "=" | \
     jq -nR 'reduce inputs as $line ({}; . + { ($line | split("=")[0]): ($line | split("=")[1:]) | join("=") })'
