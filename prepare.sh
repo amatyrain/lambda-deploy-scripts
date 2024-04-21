9976753c32ad3ddf71f08775900ca4ba8f604bcb
amatyrain <107156393+amatyrain@users.noreply.github.com>
2024-04-21 21:02:34 +0900

    lambdaレイヤーの更新

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -ex
+base=$(cd $(dirname $0); pwd)
+
+source $base/set-gh-secrets.sh
---
+++ update-lambda-layer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+base=$(cd $(dirname $0); pwd;)
+
+# 環境変数の読み込み
+source $base/lambda-deploy.sh
+
+create_lambda_layer_zip
