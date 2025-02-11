5b6e84df12a724c5a17e7744ce42ae6d96a03ee8
amatyrain <107156393+amatyrain@users.noreply.github.com>
2025-02-11 20:35:47 +0900

    update

--- terraform/modules/iam/github-actions-role.tf
+++ terraform/modules/iam/github-actions-role.tf
@@ -1,3 +1,9 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
 resource "aws_iam_role" "github_actions" {
   name = "github-actions-role"
 
@@ -11,11 +17,9 @@ resource "aws_iam_role" "github_actions" {
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub": "repo:${var.github_repository}:*"
-          }
           StringEquals = {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub": "repo:${var.github_repository}:ref:refs/heads/develop"
           }
         }
       }
